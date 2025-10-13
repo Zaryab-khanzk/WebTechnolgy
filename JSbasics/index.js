e9374c02234389f7836b02cbe6aac2109b3eff42
Zaryab-khanzk <zaryabkhan3047@gmail.com>
2025-10-13 02:37:37 -0700

    js basics

---
+++ JSbasics/index.js
@@ -0,0 +1,7 @@
+console.log('Hello world again');
+let name = 'zaryab'; //declaring a variable "let"
+let lastname = 'khan'; 
+console.log(name, lastname);
+const rate=0.3; //declaring a constant
+// rate=1; we cannot reassign a constant
+console.log(rate);
