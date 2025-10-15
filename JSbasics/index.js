2eb1fb647701680c1cf97fa0805d0cd2cc9ebf3d
Zaryab-khan <zaryabkhan3047@gmail.com>
2025-10-15 14:13:06 +0500

    practice js

--- JSbasics/index.js
+++ JSbasics/index.js
@@ -5,3 +5,9 @@ console.log(name, lastname);
 const rate=0.3; //declaring a constant
 // rate=1; we cannot reassign a constant
 console.log(rate);
+document.write("<br>");
+console.log(name);
+document.write("name");
+console.time("name");
+var text = document.getElementById("hehe").innerText;
+console.log("heelllo", text);
