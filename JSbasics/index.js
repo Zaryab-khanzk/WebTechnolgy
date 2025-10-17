e8f89e85b5c027d9b318127c1ed3ccec27f1b16c
Zaryab-khanzk <zaryabkhan3047@gmail.com>
2025-10-17 05:41:47 -0700

    unreg tasks js

--- JSbasics/index.js
+++ JSbasics/index.js
@@ -5,3 +5,10 @@ console.log(name, lastname);
 const rate=0.3; //declaring a constant
 // rate=1; we cannot reassign a constant
 console.log(rate);
+
+let name1='aemal'; // string literal
+let age=30; // number literal
+let isapproved= true; //boolean literal
+let hisname; // undefined
+let hername= null; //same as above
+let itsname= undefined; // same as above
