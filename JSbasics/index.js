ef11b184c9582ff99e03111b0e66f26035976298
Zaryab-khan <zaryabkhan3047@gmail.com>
2025-10-22 14:36:59 +0500

    task1 lab8

--- JSbasics/index.js
+++ JSbasics/index.js
@@ -5,14 +5,6 @@ console.log(name, lastname);
 const rate=0.3; //declaring a constant
 // rate=1; we cannot reassign a constant
 console.log(rate);
-<<<<<<< HEAD
-document.write("<br>");
-console.log(name);
-document.write("name");
-console.time("name");
-var text = document.getElementById("hehe").innerText;
-console.log("heelllo", text);
-=======
 
 let name1='aemal'; // string literal
 let age=30; // number literal
@@ -20,4 +12,3 @@ let isapproved= true; //boolean literal
 let hisname; // undefined
 let hername= null; //same as above
 let itsname= undefined; // same as above
->>>>>>> e8f89e85b5c027d9b318127c1ed3ccec27f1b16c
